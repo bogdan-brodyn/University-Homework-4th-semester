7772cdc68dff58e1cad84fa1537f5a8b5deb5ddd
bogdan-brodyn <mashina0oshibok@gmail.com>
2025-09-23 18:16:39 +0300

    Homework-Feb-27: Add Number-Search implementation with unit tests

---
+++ Homework-Feb-27/Number-Search/NumberSearch/NumberSearchLibrary.fs
@@ -0,0 +1,13 @@
+﻿namespace NumberSearch
+
+module NumberSearch =
+    let find n ls =
+        let rec findStep n pos ls =
+            if List.isEmpty ls then
+                -1
+            else
+                if List.head ls = n then
+                    pos
+                else
+                    findStep n (pos + 1) (List.tail ls)
+        findStep n 0 ls
---
+++ Homework-Feb-27/Number-Search/NumberSearchTests/NumberSearchTests.fs
@@ -0,0 +1,21 @@
+﻿module NumberSearchTests
+
+open NumberSearch
+open NUnit.Framework
+open FsUnit
+
+[<Test>]
+let ``Search should return -1 on empty list`` () =
+    NumberSearch.find 1 List.Empty |> should equal -1
+
+[<Test>]
+let ``Search should return -1 if list doesn't contain the number`` () =
+    NumberSearch.find 1 [0] |> should equal -1
+
+[<Test>]
+let ``Search test with zero element match`` () =
+    NumberSearch.find 1 [1] |> should equal 0
+
+[<Test>]
+let ``Search test with first element match`` () =
+    NumberSearch.find 1 [0; 1] |> should equal 1
