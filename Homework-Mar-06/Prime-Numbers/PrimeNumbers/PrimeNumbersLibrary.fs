6f75b0eaed1420fdf09d9853eb0b1eb96b64dbdb
bogdan-brodyn <mashina0oshibok@gmail.com>
2025-10-27 02:16:25 +0300

    Homework-Mar-06: Add Prime-Numbers implementation with unit tests

---
+++ Homework-Mar-06/Prime-Numbers/PrimeNumbers/PrimeNumbersLibrary.fs
@@ -0,0 +1,16 @@
+﻿namespace PrimeNumbers
+
+module PrimeNumbers =
+    let getSequence =
+        let isPrime n =
+            let rec isPrimeStep currentStep=
+                if currentStep = n then
+                    true
+                else if n % currentStep = 0 then
+                    false
+                else
+                    isPrimeStep (currentStep + 1)
+            isPrimeStep 2
+
+        let naturalNumbersFromTwo = Seq.initInfinite (fun i -> i + 1)
+        naturalNumbersFromTwo |> Seq.filter isPrime 
