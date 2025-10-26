f72577d0bb47b6deb3dd2542ff3d0eb4b358a1a2
bogdan-brodyn <mashina0oshibok@gmail.com>
2025-10-26 21:32:52 +0300

    Homework-Feb-27: Add Reverse-List implementation with unit tests

---
+++ Homework-Feb-27/Reverse-List/ReverseListTests/ReverseListTests.fs
@@ -0,0 +1,17 @@
+﻿module ReverseListTests
+
+open ReverseList
+open NUnit.Framework
+open FsUnit
+
+[<Test>]
+let ``Reverse should return an empty list on empty list`` () =
+    ReverseList.reverse List.Empty |> should equal List.Empty
+
+[<Test>]
+let ``Reverse should return the same list on one element list`` () =
+    ReverseList.reverse [0] |> should equal [0]
+
+[<Test>]
+let ``Reverse usual test case`` () =
+    ReverseList.reverse [1; 2; 3] |> should equal [3; 2; 1]
