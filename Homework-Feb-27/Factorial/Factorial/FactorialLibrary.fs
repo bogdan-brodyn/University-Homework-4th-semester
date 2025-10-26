4ea2129039c19bf17aab99731db367e3865a6c93
bogdan-brodyn <mashina0oshibok@gmail.com>
2025-10-26 22:51:23 +0300

    Homework-Feb-27: Add Factorial implementation with unit tests

---
+++ Homework-Feb-27/Factorial/Factorial/FactorialLibrary.fs
@@ -0,0 +1,12 @@
+﻿namespace Factorial
+
+module Factorial =
+    let compute n =
+        let rec computeStep currentStep acc =
+            if n < currentStep then
+                acc
+            else
+                computeStep (currentStep + 1) (currentStep * acc)
+        if n < 0 then
+            invalidArg "n" "Argument 'n' cannot be negative!!!"
+        computeStep 1 1
