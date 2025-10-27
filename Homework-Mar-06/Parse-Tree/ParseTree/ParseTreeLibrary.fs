30ac1788b7214095577d4cee86c756c40e01e12b
bogdan-brodyn <mashina0oshibok@gmail.com>
2025-10-27 23:40:57 +0300

    Homework-Mar-06: Add Parse-Tree implementation with unit tests

---
+++ Homework-Mar-06/Parse-Tree/ParseTree/ParseTreeLibrary.fs
@@ -0,0 +1,12 @@
+﻿namespace ParseTree
+
+type ParseTree =
+    | Tree of (int -> int -> int) * ParseTree * ParseTree
+    | Leaf of int
+
+module ParseTree =
+    let rec compute parseTree =
+        match parseTree with
+        | Tree(operator, leftSubtree, rightSubtree) ->
+            operator (compute leftSubtree) (compute rightSubtree)
+        | Leaf(value) -> value
