eb76cf9974d430cd187d87e3351d72bfa20b7ae7
bogdan-brodyn <mashina0oshibok@gmail.com>
2025-12-02 01:54:56 +0300

    Homework-Mar-20: Add Parenthesis-Sequence implementation with unit tests

---
+++ Homework-Mar-20/Parenthesis-Sequence/ParenthesisSequence/ParenthesisSequenceLibrary.fs
@@ -0,0 +1,25 @@
+﻿namespace ParenthesisSequence
+
+module ParenthesisSequence =
+    let isValidParenthesisSequence str =
+        let isValidParenthesisPair left right =
+            left = '(' && right = ')'
+            || left = '{' && right = '}'
+            || left = '[' && right = ']'
+
+        let rec isValidParenthesisSequenceEnding str position stack=
+            if String.length str <= position then
+                List.isEmpty stack
+            else
+                let currentChar = str[position]
+                match currentChar with
+                    | '(' | '{' | '[' -> isValidParenthesisSequenceEnding str (position + 1) (currentChar :: stack)
+                    | ')' | '}' | ']' -> 
+                        match stack with
+                            | lastOpenParenthesis :: tail ->
+                                isValidParenthesisPair lastOpenParenthesis currentChar
+                                    && isValidParenthesisSequenceEnding str (position + 1) tail
+                            | [] -> false
+                    | _ -> isValidParenthesisSequenceEnding str (position + 1) stack
+
+        isValidParenthesisSequenceEnding str 0 List.empty
