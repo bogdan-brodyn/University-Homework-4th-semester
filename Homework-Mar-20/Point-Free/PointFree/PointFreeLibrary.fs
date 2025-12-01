d43ad66c07b86094d62e15d7b9ad61c88da93006
bogdan-brodyn <mashina0oshibok@gmail.com>
2025-12-02 02:00:42 +0300

    Homework-Mar-20: Add Point-Free implementation with equivalence tests

---
+++ Homework-Mar-20/Point-Free/PointFree/PointFreeLibrary.fs
@@ -0,0 +1,20 @@
+﻿namespace PointFree
+
+module PointFree =
+    // initial func
+    let func x l = List.map (fun y -> y * x) l
+
+    // applied eta conversion
+    let func'1 x = List.map (fun y -> y * x)
+
+    // pull 'y' to the rightmost position
+    let func'2 x = List.map (fun y -> (*) x y)
+
+    // applied eta conversion
+    let func'3 x = List.map ((*) x)
+
+    // used pipeline
+    let func'4 x = x |> (*) |> List.map
+
+    // applied eta conversion
+    let func'5 = (*) >> List.map 
---
+++ Homework-Mar-20/Point-Free/PointFreeTests/PointFreeEquivalenceTests.fs
@@ -0,0 +1,25 @@
+﻿module PointFreeEquivalenceTests
+
+open PointFree
+open NUnit.Framework
+open FsCheck
+
+[<Test>]
+let ``func and func'1 should be equivalent`` () =
+    Check.QuickThrowOnFailure (fun x l -> PointFree.func x l = PointFree.func'1 x l)
+
+[<Test>]
+let ``func'1 and func'2 should be equivalent`` () =
+    Check.QuickThrowOnFailure (fun x l -> PointFree.func'1 x l = PointFree.func'2 x l)
+
+[<Test>]
+let ``func'2 and func'3 should be equivalent`` () =
+    Check.QuickThrowOnFailure (fun x l -> PointFree.func'2 x l = PointFree.func'3 x l)
+
+[<Test>]
+let ``func'3 and func'4 should be equivalent`` () =
+    Check.QuickThrowOnFailure (fun x l -> PointFree.func'3 x l = PointFree.func'4 x l)
+
+[<Test>]
+let ``func'4 and func'5 should be equivalent`` () =
+    Check.QuickThrowOnFailure (fun x l -> PointFree.func'4 x l = PointFree.func'5 x l)
