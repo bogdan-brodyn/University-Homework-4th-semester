167527ca42a7b127305c5adf680fed6c704f75ad
bogdan-brodyn <mashina0oshibok@gmail.com>
2025-12-06 10:19:02 +0300

    Homework-Feb-06: Resolve conversations

--- Homework-Mar-06/Prime-Numbers/PrimeNumbers/PrimeNumbersLibrary.fs
+++ Homework-Mar-06/Prime-Numbers/PrimeNumbers/PrimeNumbersLibrary.fs
@@ -1,10 +1,14 @@
 ﻿namespace PrimeNumbers
 
+open System
+
 module PrimeNumbers =
+    // returns infinite sequence of prime numbers
     let getSequence =
-        let isPrime n =
-            let rec isPrimeStep currentStep=
-                if currentStep = n then
+        let isPrime (n : int) =
+            let dividerUpperBound = n |> Math.Sqrt |> int
+            let rec isPrimeStep currentStep =
+                if currentStep > dividerUpperBound then
                     true
                 else if n % currentStep = 0 then
                     false
@@ -12,5 +16,6 @@ module PrimeNumbers =
                     isPrimeStep (currentStep + 1)
             isPrimeStep 2
 
-        let naturalNumbersFromTwo = Seq.initInfinite (fun i -> i + 1)
-        naturalNumbersFromTwo |> Seq.filter isPrime 
+        (fun i -> i + 2)
+            |> Seq.initInfinite // integer numbers from 2
+            |> Seq.filter isPrime 
